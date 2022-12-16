17bb35ba87c177b8c447c96ac0af433c61ce5872
Wataru Yasuda <wataru.robo@gmail.com>
2022-12-16 23:16:29 +0900

    with error2

--- include/plateau/geometry/geo_reference.h
+++ include/plateau/geometry/geo_reference.h
@@ -15,31 +15,31 @@ namespace plateau::geometry {
      */
     class LIBPLATEAU_EXPORT GeoReference {
     public:
-        explicit GeoReference(int coordinate_zone_id, const TVec3d& reference_point = TVec3d(0, 0, 0),
+        explicit GeoReference(int coordinate_zone_id, const Vector3d& reference_point = Vector3d(0, 0, 0),
                               float unit_scale = 1.0, CoordinateSystem coordinate_system = CoordinateSystem::ENU);
 
         /**
          * 緯度・経度・高さで表現される座標を平面直角座標系に変換します。
          * 座標軸変換を含みます。
          */
-        TVec3d project(const GeoCoordinate& point) const;
-        TVec3d project(const TVec3d& lat_lon) const;
+        Vector3d project(const GeoCoordinate& point) const;
+        Vector3d project(const Vector3d& lat_lon) const;
         /// project の座標軸変換をしない版です。座標軸は ENU → ENU であるとします。 reference_point_ は ENUに変換されます。
-        TVec3d projectWithoutAxisConvert(const TVec3d& lat_lon) const;
-        static TVec3d convertAxisFromENUTo(CoordinateSystem axis, const TVec3d& vertex);
-        static TVec3d convertAxisToENU(CoordinateSystem axis, const TVec3d& vertex);
+        Vector3d projectWithoutAxisConvert(const Vector3d& lat_lon) const;
+        static Vector3d convertAxisFromENUTo(CoordinateSystem axis, const Vector3d& vertex);
+        static Vector3d convertAxisToENU(CoordinateSystem axis, const Vector3d& vertex);
 
-        GeoCoordinate unproject(const TVec3d& point) const;
+        GeoCoordinate unproject(const Vector3d& point) const;
 
-        void setReferencePoint(TVec3d point);
-        TVec3d getReferencePoint() const;
+        void setReferencePoint(Vector3d point);
+        Vector3d getReferencePoint() const;
         int getZoneID() const;
         void setZoneID(int value);
         float getUnitScale() const;
         CoordinateSystem getCoordinateSystem() const;
 
     private:
-        TVec3d reference_point_;
+        Vector3d reference_point_;
         int zone_id_;
         CoordinateSystem coordinate_system_;
         float unit_scale_;
--- include/plateau/geometry/vectors.h
+++ include/plateau/geometry/vectors.h
@@ -7,7 +7,7 @@
 namespace plateau::geometry {
     // 3D vector class.
 
-    template< class T > class TVec3 {
+    template< class T > class Vector3 {
     public:
         union {
             T xyz[3];
@@ -21,31 +21,31 @@ namespace plateau::geometry {
         };
 
     public:
-        TVec3(const T x = (T)0, const T y = (T)0, const T z = (T)0);
-        TVec3(const T vec[]);
+        Vector3(const T x = (T)0, const T y = (T)0, const T z = (T)0);
+        Vector3(const T vec[]);
 
         inline T length() const;
         inline T sqrLength() const;
 
-        T dot(const TVec3<T>& vec) const;
-        TVec3 cross(const TVec3<T>& vec) const;
+        T dot(const Vector3<T>& vec) const;
+        Vector3 cross(const Vector3<T>& vec) const;
 
-        TVec3  normal() const;
-        TVec3& normalEq();
-        TVec3& normalEq(const T length);
+        Vector3  normal() const;
+        Vector3& normalEq();
+        Vector3& normalEq(const T length);
 
-        TVec3 operator+(const TVec3<T>& rhs) const;
-        TVec3 operator+(const T& _v) const;
-        TVec3 operator-(const TVec3<T>& rhs) const;
-        TVec3 operator-(const T& _v) const;
-        TVec3 operator-() const;
-        TVec3 operator*(const T& rhs) const;
-        TVec3 operator*(const TVec3<T>& rhs) const;
-        TVec3 operator/(const T& rhs) const;
-        TVec3 operator/(const TVec3<T>& rhs) const;
+        Vector3 operator+(const Vector3<T>& rhs) const;
+        Vector3 operator+(const T& _v) const;
+        Vector3 operator-(const Vector3<T>& rhs) const;
+        Vector3 operator-(const T& _v) const;
+        Vector3 operator-() const;
+        Vector3 operator*(const T& rhs) const;
+        Vector3 operator*(const Vector3<T>& rhs) const;
+        Vector3 operator/(const T& rhs) const;
+        Vector3 operator/(const Vector3<T>& rhs) const;
 
-        inline bool operator==(const TVec3<T>& rhs) const;
-        inline bool operator!=(const TVec3<T>& rhs) const;
+        inline bool operator==(const Vector3<T>& rhs) const;
+        inline bool operator!=(const Vector3<T>& rhs) const;
 
         inline operator T* () {
             return xyz;
@@ -55,39 +55,39 @@ namespace plateau::geometry {
         }
     };
 
-    template< class T > inline TVec3<T>::TVec3(const T x, const T y, const T z) {
+    template< class T > inline Vector3<T>::Vector3(const T x, const T y, const T z) {
         this->x = x;
         this->y = y;
         this->z = z;
     }
 
-    template< class T >	inline TVec3<T>::TVec3(const T vec[]) {
+    template< class T >	inline Vector3<T>::Vector3(const T vec[]) {
         memcpy(xyz, vec, 3 * sizeof(T));
     }
 
-    template< class T > inline T TVec3<T>::length() const {
+    template< class T > inline T Vector3<T>::length() const {
         return (T)sqrt(x * x + y * y + z * z);
     }
 
-    template< class T > inline T TVec3<T>::sqrLength() const {
+    template< class T > inline T Vector3<T>::sqrLength() const {
         return x * x + y * y + z * z;
     }
 
-    template< class T > inline T TVec3<T>::dot(const TVec3<T>& vec) const {
+    template< class T > inline T Vector3<T>::dot(const Vector3<T>& vec) const {
         return x * vec.x + y * vec.y + z * vec.z;
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::cross(const TVec3<T>& vec) const {
-        return TVec3<T>(y * vec.z - z * vec.y, z * vec.x - x * vec.z, x * vec.y - y * vec.x);
+    template< class T > inline Vector3<T> Vector3<T>::cross(const Vector3<T>& vec) const {
+        return Vector3<T>(y * vec.z - z * vec.y, z * vec.x - x * vec.z, x * vec.y - y * vec.x);
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::normal() const {
+    template< class T > inline Vector3<T> Vector3<T>::normal() const {
         const T len = length();
         const T tmp = (len != (T)0) ? ((T)1 / len) : (T)0;
-        return TVec3<T>(x * tmp, y * tmp, z * tmp);
+        return Vector3<T>(x * tmp, y * tmp, z * tmp);
     }
 
-    template< class T > inline TVec3<T>& TVec3<T>::normalEq() {
+    template< class T > inline Vector3<T>& Vector3<T>::normalEq() {
         const T len = length();
         const T tmp = (len != (T)0) ? ((T)1 / len) : (T)0;
         x *= tmp;
@@ -96,8 +96,8 @@ namespace plateau::geometry {
         return *this;
     }
 
-    template< class T > inline TVec3<T>& TVec3<T>::normalEq(const T length) {
-        const T len = TVec3<T>::length();
+    template< class T > inline Vector3<T>& Vector3<T>::normalEq(const T length) {
+        const T len = Vector3<T>::length();
         const T tmp = (len != (T)0) ? length / len : (T)0;
         x *= tmp;
         y *= tmp;
@@ -105,63 +105,63 @@ namespace plateau::geometry {
         return *this;
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::operator+(const TVec3<T>& rhs) const {
-        return TVec3<T>(x + rhs.x, y + rhs.y, z + rhs.z);
+    template< class T > inline Vector3<T> Vector3<T>::operator+(const Vector3<T>& rhs) const {
+        return Vector3<T>(x + rhs.x, y + rhs.y, z + rhs.z);
     }
 
-    template< class T >	inline TVec3<T> TVec3<T>::operator+(const T& _v) const {
-        return TVec3<T>(x + _v, y + _v, z + _v);
+    template< class T >	inline Vector3<T> Vector3<T>::operator+(const T& _v) const {
+        return Vector3<T>(x + _v, y + _v, z + _v);
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::operator-(const TVec3<T>& rhs) const {
-        return TVec3<T>(x - rhs.x, y - rhs.y, z - rhs.z);
+    template< class T > inline Vector3<T> Vector3<T>::operator-(const Vector3<T>& rhs) const {
+        return Vector3<T>(x - rhs.x, y - rhs.y, z - rhs.z);
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::operator-(const T& _v) const {
-        return TVec3<T>(x - _v, y - _v, z - _v);
+    template< class T > inline Vector3<T> Vector3<T>::operator-(const T& _v) const {
+        return Vector3<T>(x - _v, y - _v, z - _v);
     }
 
-    template< class T >	inline TVec3<T> TVec3<T>::operator-() const {
-        return TVec3<T>(-x, -y, -z);
+    template< class T >	inline Vector3<T> Vector3<T>::operator-() const {
+        return Vector3<T>(-x, -y, -z);
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::operator*(const T& rhs) const {
-        return TVec3<T>(x * rhs, y * rhs, z * rhs);
+    template< class T > inline Vector3<T> Vector3<T>::operator*(const T& rhs) const {
+        return Vector3<T>(x * rhs, y * rhs, z * rhs);
     }
 
-    template< class T >	inline TVec3<T> TVec3<T>::operator*(const TVec3<T>& rhs) const {
-        return TVec3<T>(x * rhs.x, y * rhs.y, z * rhs.z);
+    template< class T >	inline Vector3<T> Vector3<T>::operator*(const Vector3<T>& rhs) const {
+        return Vector3<T>(x * rhs.x, y * rhs.y, z * rhs.z);
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::operator/(const T& rhs) const {
-        return TVec3<T>(x / rhs, y / rhs, z / rhs);
+    template< class T > inline Vector3<T> Vector3<T>::operator/(const T& rhs) const {
+        return Vector3<T>(x / rhs, y / rhs, z / rhs);
     }
 
-    template< class T > inline TVec3<T> TVec3<T>::operator/(const TVec3<T>& rhs) const {
-        return TVec3<T>(x / rhs.x, y / rhs.y, z / rhs.z);
+    template< class T > inline Vector3<T> Vector3<T>::operator/(const Vector3<T>& rhs) const {
+        return Vector3<T>(x / rhs.x, y / rhs.y, z / rhs.z);
     }
 
-    template< class T > inline bool TVec3<T>::operator==(const TVec3<T>& rhs) const {
+    template< class T > inline bool Vector3<T>::operator==(const Vector3<T>& rhs) const {
         return x == rhs.x && y == rhs.y && z == rhs.z;
     }
 
-    template< class T > inline bool TVec3<T>::operator!=(const TVec3<T>& rhs) const {
+    template< class T > inline bool Vector3<T>::operator!=(const Vector3<T>& rhs) const {
         return x != rhs.x || y != rhs.y || z != rhs.z;
     }
 
-    template< class T >	inline TVec3<T> operator*(const T& val, const TVec3<T>& vec) {
-        return TVec3<T>(vec.x * val, vec.y * val, vec.z * val);
+    template< class T >	inline Vector3<T> operator*(const T& val, const Vector3<T>& vec) {
+        return Vector3<T>(vec.x * val, vec.y * val, vec.z * val);
     }
 
-    template<class T> inline std::ostream& operator<<(std::ostream& os, const TVec3<T>& v) {
+    template<class T> inline std::ostream& operator<<(std::ostream& os, const Vector3<T>& v) {
         return os << std::fixed << v.x << " " << std::fixed << v.y << " " << std::fixed << v.z;
     }
 
-    template<class T> inline std::istream& operator>>(std::istream& is, TVec3<T>& v) {
+    template<class T> inline std::istream& operator>>(std::istream& is, Vector3<T>& v) {
         return is >> v.x >> v.y >> v.z;
     }
 
-    typedef TVec3< float >			TVec3f;
-    typedef TVec3< double >			TVec3d;
+    typedef Vector3< float >			Vector3f;
+    typedef Vector3< double >			Vector3d;
 
 }
